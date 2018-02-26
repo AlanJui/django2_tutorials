1d3291a6e943feae244a59bf675272469f42a97d
居正中 <AlanJui.1960@gmail.com>
2018-02-26 22:50:42 +0800

    Display view: polls/index.html with shortcuty: render()

--- polls/views.py
+++ polls/views.py
@@ -7,12 +7,9 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    template = loader.get_template('polls/index.html')
-    context = {
+    return render(request, 'polls/index.html', context= {
         'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    })
 
 def detail(request, question_id):
     return HttpResponse("You're looking a question %s." % question_id)
