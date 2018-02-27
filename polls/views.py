b718f6fa189c27bae3a2944903a41ccb003e0de7
居正中 <AlanJui.1960@gmail.com>
2018-02-27 15:33:43 +0800

    使用 Http404() 方法，進行資料找不到時的意外處理

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import  HttpResponse
+from django.http import  HttpResponse, Http404
 
 from .models import Question
 
@@ -12,7 +12,14 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return HttpResponse("You're looking a question %s." % question_id)
+    # return HttpResponse("You're looking a question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
